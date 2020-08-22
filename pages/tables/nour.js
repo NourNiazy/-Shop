fd42452f2f544c8d74ff3c7e437aca9147e7fb45
NourNiazy <nourmostafa01551@gmail.com>
2020-08-22 16:55:05 +0300

    ......

---
+++ pages/tables/nour.js
@@ -0,0 +1,10 @@
+/*problem solution
+$('#example1').dataTable( {
+    "scrollY": "200px"
+  } );
+   
+  // Some time later, recreate without (no scrolling)
+  $('#example1').dataTable( {
+    "destroy": true
+  } );
+  */
